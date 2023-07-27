b74384832c1e4b4c65500bd8115c7f544a9d80f3
Oliver Hees <oliverhees@gmail.com>
2023-07-27 12:06:19 +0200

    add training plan button

---
+++ components/CreateTrainingButton/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default function CreateTrainingButton() {
+  return (
+    <CreateTrainingButtonDiv>Create training plan</CreateTrainingButtonDiv>
+  );
+}
+
+const CreateTrainingButtonDiv = styled.button`
+  width: 100%;
+`;
