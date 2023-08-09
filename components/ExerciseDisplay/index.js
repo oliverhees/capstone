8ad91ebbe8c2aaccd5a246d3224ffc3207542833
Oliver Hees <oliverhees@gmail.com>
2023-08-09 16:47:21 +0200

    fix error

--- components/ExerciseDisplay/index.js
+++ components/ExerciseDisplay/index.js
@@ -1,4 +1,4 @@
-import { use, useState } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import { GrFormPreviousLink, GrFormNextLink } from "react-icons/gr";
 import Link from "next/link";
